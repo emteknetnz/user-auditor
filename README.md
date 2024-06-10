e617d82669453a6d527bfeb0ec0776f80f300c38
Steve Boyd <emteknetnz@gmail.com>
2024-06-10 17:11:27 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# user-auditor
+
+Create reports of groups and user on github
+
+## Token
+
+Create a classic token on github and tick the "repo" checkbgox which will automatically tick the checkboxes below it
+
+https://github.com/settings/tokens/new
+
+## Usage
+
+TOKEN=abc run.php
---
+++ run.php
@@ -0,0 +1,40 @@
+<?php
+
+function github_api($url, $data = [], $httpMethod = '') {
+    // silverstripe-themes has a kind of weird redirect only for api requests
+    $url = str_replace('/silverstripe-themes/silverstripe-simple', '/silverstripe/silverstripe-simple', $url);
+    $method = $httpMethod ? strtoupper($httpMethod) : 'GET';
+    print("Making $method curl request to $url\n");
+    $token = getenv('TOKEN');
+    $jsonStr = empty($data) ? '' : json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, !empty($data));
+    if ($httpMethod) {
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $httpMethod);
+    }
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'User-Agent: PHP script',
+        'Accept: application/vnd.github+json',
+        "Authorization: Bearer $token",
+        'X-GitHub-Api-Version: 2022-11-28'
+    ]);
+    if ($jsonStr) {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+    }
+    $response = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if ($httpcode >= 300) {
+        print("HTTP code $httpcode returned from GitHub API\n");
+        print("$response\n");
+        print("Failure calling github api: $url\n");
+        exit;
+    }
+    return json_decode($response, true);
+}
+
+if (!getenv('TOKEN')) {
+    print("Set the TOKEN environment variable\n");
+    exit;
+}
